d3a2ff5c7218f1f10655b356a90ab3fff6616315
pavansrinivas <pskatragadda@sysintelli.com>
2019-07-22 16:41:22 +0530

    commit

--- 05-Spring_JDBC/src/main/java/com/sysintelli/application/Application.java
+++ 05-Spring_JDBC/src/main/java/com/sysintelli/application/Application.java
@@ -1,28 +1,33 @@
 package com.sysintelli.application;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.sysintelli.dao.EmployeeDAO;
+import com.sysintelli.entity.Employee;
 
 public class Application {
 
 	public static void main(String[] args) throws SQLException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring_config.xml");
-		DriverManagerDataSource dataSource = context.getBean(DriverManagerDataSource.class);
-		Connection con = dataSource.getConnection();
-		Statement stmt = con.createStatement();
-		
-		ResultSet rs = stmt.executeQuery("select * from employee");
-		while(rs.next()) {
-			System.out.println(rs.getString(1)+";"+rs.getString(2)+";"+rs.getString(3));
+		EmployeeDAO e = (EmployeeDAO) context.getBean("empDao");
+		Employee emp= (Employee) context.getBean("empid");
+		//int id = emp.getId();
+		//e.getEmployeeByID(id);
+	//System.out.println("emp");
+	//System.out.println(e.getEmployeeByID(id));
+		List<Employee>Ems= new ArrayList<Employee>();
+		Ems =e.getAllEmployees();//
+	for( Employee et:Ems){
+		System.out.println("ID:"+et.getId());
+		System.out.println("name :"+et.getName());
+		System.out.println("salary :"+et.getSalary());
 		}
-		
-		con.close();
-	}
+
+			}
 
 }
